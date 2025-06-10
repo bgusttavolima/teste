78bfe0350ebe8035826aed787948528868cde23e
bgusttavolima <clebermach299@gmail.com>
2025-06-10 14:27:32 +0000

    foi arrumado o .env e o bug foi arrumado, comentei o ocodigo do db.js

--- node-postgresql/db.js
+++ node-postgresql/db.js
@@ -1,22 +1,32 @@
 async function connect() {
 
+    //aqui e uma verificacao para nao deixar duplicar a conexao
     if(global.connection)
         return global.connection.connect();
 
-
+    // Aqui está importando a classe Pool do pacote pg, que é o driver de PostgreSQL para Node.js
     const {Pool} = require("pg");
+    //aqui ele cria um pool de conexao com a string de conexao do projeto
     const pool = new Pool({
         connectionString: process.env.CONNECTION_STRING
 
     });
+    //aqui tem uma funcao await que vai esperar a conexao ser bem sucedida e devolver um console.log
     const client = await pool.connect();
     console.log("criou o pool de conexao");
 
+    //aqui ele vai dar um client.query na data do banco e vai usar res.
+    // rows[0] para especificar a informacao que vai ser pega
     const res = await client.query("select now()");
     console.log(res.rows[0]);
+
+    //Libera essa conexão para que o pool possa reutilizá-la no futuro.
     client.release();
     
+    //Salva o pool de conexões na variável global para que ele possa ser reutilizado.
     global.connection = pool;
+
+    //Retorna uma nova conexão do pool para quem chamou a função.
     return pool.connect();
 }
 
